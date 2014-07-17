b44e21b75b19ebee8dd0cca9c1dac08cc33f9c00
cassiodsl <cassiodsl@gmail.com>
2014-07-16 21:24:12 -0300

    primeira versão

---
+++ src/com/jovens/system/Iniciar.java
@@ -0,0 +1,17 @@
+
+package com.jovens.system;
+
+/**
+ *
+ * @author Cassio
+ */
+public class Iniciar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
